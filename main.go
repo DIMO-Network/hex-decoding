73da9acdc13185652b53b0bb2536842735a5f2ed
lauradeng23 <140633925+lauradeng23@users.noreply.github.com>
2023-10-19 11:19:59 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,88 +1,51 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 func ExtractAndDecodeWithFormula(hexData string, formula string) (float64, string, error) {
-
 	// Define a regex for the formula
-	re := regexp.MustCompile(`(\d+)\|(.+)`)
-
+	re := regexp.MustCompile(`(\d+)\|(\d+)@(\d+)\+ \(([^,]+),([^)]+)\) \[([^|]+)\|([^]]+)] "([^"]+)"`)
 	matches := re.FindStringSubmatch(formula)
 
-	if len(matches) != 3 {
+	if len(matches) != 9 {
 		return 0, "", fmt.Errorf("invalid formula format: %s", formula)
 	}
 
-	offset, err := strconv.Atoi(matches[1])
+	startBit, err := strconv.Atoi(matches[1])
 	if err != nil {
 		return 0, "", err
 	}
-
-	formulaParts := strings.Fields(matches[2])
-
-	//formulaParts[0] contains length, formulaParts[1] contains factor parts, formulaParts[2] contains range parts, formulaParts[3] contains the units
-	if len(formulaParts) != 4 {
-		return 0, "", fmt.Errorf("invalid formula format: %s", formula)
-	}
-
-	// Extract length, scale factor, and offset adjustment values
-	lengthPart := extractNumericPart(formulaParts[0])
-	length, err := strconv.Atoi(lengthPart)
+	lengthBits, err := strconv.Atoi(matches[2])
 	if err != nil {
 		return 0, "", err
 	}
-
-	// Parse scaling factor and offset adjustment
-	factorPart := strings.Trim(formulaParts[1], " ")
-
-	// Extract scaleFactor and offsetAdjustment from factorPart
-	scaleFactorIndex := strings.Index(factorPart, "(")
-	offsetAdjustmentIndex := strings.Index(factorPart, ",")
-	if scaleFactorIndex == -1 || offsetAdjustmentIndex == -1 {
-		return 0, "", fmt.Errorf("invalid factor format: %s", factorPart)
-	}
-
-	scaleFactorStr := factorPart[scaleFactorIndex+1 : offsetAdjustmentIndex]
-	offsetAdjustmentStr := factorPart[offsetAdjustmentIndex+1 : len(factorPart)-1]
-
-	// Convert scaleFactorStr and offsetAdjustmentStr to float64
-	scaleFactor, err := strconv.ParseFloat(strings.Trim(scaleFactorStr, " "), 64)
+	endianness, err := strconv.Atoi(matches[3])
 	if err != nil {
 		return 0, "", err
 	}
-
-	offsetAdjustment, err := strconv.ParseFloat(strings.Trim(offsetAdjustmentStr, " "), 64)
+	scaleFactor, err := strconv.ParseFloat(matches[4], 64)
 	if err != nil {
 		return 0, "", err
 	}
-
-	rangePart := strings.Trim(formulaParts[2], "")
-	minValueIndex := strings.Index(rangePart, "[")
-	maxValueIndex := strings.Index(rangePart, "|")
-	if minValueIndex == -1 || maxValueIndex == -1 {
-		return 0, "", fmt.Errorf("invalid factor format: %s", rangePart)
-	}
-	minValueStr := rangePart[minValueIndex+1 : maxValueIndex]
-	maxValueStr := rangePart[maxValueIndex+1 : len(rangePart)-1]
-
-	// Convert minValueStr and maxValueStr to float64
-	minValue, err := strconv.ParseFloat(strings.Trim(minValueStr, " "), 64)
+	offsetAdjustment, err := strconv.ParseFloat(matches[5], 64)
 	if err != nil {
 		return 0, "", err
 	}
-
-	maxValue, err := strconv.ParseFloat(strings.Trim(maxValueStr, " "), 64)
+	minValue, err := strconv.ParseFloat(matches[6], 64)
 	if err != nil {
 		return 0, "", err
 	}
-
-	unit := formulaParts[3]
+	maxValue, err := strconv.ParseFloat(matches[7], 64)
+	if err != nil {
+		return 0, "", err
+	}
+	unit := matches[8]
 
 	// Decode hex
 	hexBytes, err := hex.DecodeString(hexData)
@@ -90,38 +53,42 @@ func ExtractAndDecodeWithFormula(hexData string, formula string) (float64, strin
 		return 0, "", err
 	}
 
-	// Check if the byte slice is long enough
-	if offset+length > len(hexBytes) {
-		return 0, "", fmt.Errorf("formula for offset %d and length %d is out of bounds", offset, length)
+	// Calculate start byte, end byte
+	startByte := startBit / 8
+	lengthBytes := (lengthBits + 7) / 8 //
+	endByte := startByte + lengthBytes
+	if endByte > len(hexBytes) {
+		return 0, "", fmt.Errorf("data out of range: data length is %d, but endByte is %d", len(hexBytes), endByte)
 	}
 
-	// Extract relevant bytes
-	hexValue := hexBytes[offset : offset+length]
-	intValue, err := strconv.ParseInt(string(hexValue), 16, 64)
-	if err != nil {
-		return 0, "", err
+	// Extract data and convert to uint32 with endianness
+	var dataUint32 uint32
+	dataBytes := hexBytes[startByte:endByte]
+	if endianness == 1 {
+		dataUint32 = binary.BigEndian.Uint32(append(make([]byte, 4-len(dataBytes)), dataBytes...)) // big-endian
+	} else {
+		dataUint32 = binary.LittleEndian.Uint32(append(dataBytes, make([]byte, 4-len(dataBytes))...)) // little-endian
 	}
+
+	// Create mask and shift bits
+	mask := (1<<lengthBits - 1) << (startBit % 8)
+	value := (dataUint32 & uint32(mask)) >> (startBit % 8)
 
 	// Apply formula components
-	decodedValue := (float64(intValue) + offsetAdjustment) * scaleFactor
+	decodedValue := (float64(value) * scaleFactor) + offsetAdjustment
 
 	// Check if the decoded value is within specified range
 	if decodedValue < minValue || decodedValue > maxValue {
-		return 0, "", fmt.Errorf("decoded value is out of range: %.2f", decodedValue)
+		return 0, "", fmt.Errorf("decoded value out of range: %.2f (expected range %.2f to %.2f)", decodedValue, minValue, maxValue)
 	}
 
 	return decodedValue, unit, nil
 }
-func extractNumericPart(s string) string {
-	re := regexp.MustCompile(`(\d+)`)
-	match := re.FindString(s)
-	return match
-}
 
 func main() {
-	// Example
-	hexData := "0123"
-	formula := "31|16@0+ (1,0) [0|65535] \"km\" "
+	// Example odometer
+	hexData := "7e80641a6000e2a2"
+	formula := "31|32@0+ (0.1,0) [1|429496730] \"km\""
 
 	decodedValue, unit, err := ExtractAndDecodeWithFormula(hexData, formula)
 	if err != nil {
