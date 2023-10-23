09a073c805bb2aa38bb749ea2de1df260cee7f61
Laura Deng <lauradeng@Lauras-MacBook-Pro.local>
2023-10-23 14:04:34 -0500

    extract hex after pid

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-// convertHexToBinary converts hexadecimal string to binary string
-func convertHexToBinary(hexData string) (string, error) {
-	bytes, err := hex.DecodeString(hexData)
-	if err != nil {
-		return "", err
-	}
-
-	var binaryData string
-	for _, b := range bytes {
-		binaryData += fmt.Sprintf("%08b", b)
-	}
-	return binaryData, nil
-}
-
-// extractBits extracts the desired sequence of bits specified by the formula
-func extractBits(binaryData string, startBit, length int) (string, error) {
-	if startBit+length > len(binaryData) {
-		return "", errors.New("out of range")
-	}
-	return binaryData[startBit : startBit+length], nil
-}
-
-// binaryStringToDecimal converts binary string to decimal
-func binaryStringToDecimal(binaryString string) (uint64, error) {
-	return strconv.ParseUint(binaryString, 2, 64)
-}
-
-func ExtractAndDecodeWithFormula(hexData string, formula string) (float64, string, error) {
-	// Regular expression to parse the formula
+// extractAndDecodeWithFormula extracts the data following the PID and applies the formula for decoding
+func extractAndDecodeWithFormula(hexData, pid, formula string) (float64, string, error) {
+	// Parse formula
 	re := regexp.MustCompile(`(\d+)\|(\d+)@(\d+)\+ \(([^,]+),([^)]+)\) \[([^|]+)\|([^]]+)] "([^"]+)"`)
 	matches := re.FindStringSubmatch(formula)
 
@@ -44,40 +18,56 @@ func ExtractAndDecodeWithFormula(hexData string, formula string) (float64, strin
 		return 0, "", fmt.Errorf("invalid formula format: %s", formula)
 	}
 
-	startBit, _ := strconv.Atoi(matches[1])
-	lengthBits, _ := strconv.Atoi(matches[2])
-	scaleFactor, _ := strconv.ParseFloat(matches[4], 64)
-	offsetAdjustment, _ := strconv.ParseFloat(matches[5], 64)
-	minValue, _ := strconv.ParseFloat(matches[6], 64)
-	maxValue, _ := strconv.ParseFloat(matches[7], 64)
+	lengthBits, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return 0, "", err
+	}
+	numBytes := lengthBits / 8
+
+	// Find the index of PID in the hex string
+	pidIndex := strings.Index(hexData, pid)
+	if pidIndex == -1 {
+		return 0, "", errors.New("PID not found")
+	}
+
+	// Calculate the start index of the data after the PID
+	dataStartIndex := pidIndex + len(pid)
+
+	if dataStartIndex+numBytes*2 > len(hexData) {
+		return 0, "", errors.New("not enough data")
+	}
+
+	// Extract the relevant portion of the hex data
+	valueHex := hexData[dataStartIndex : dataStartIndex+numBytes*2]
+
+	value, err := strconv.ParseUint(valueHex, 16, 64)
+	if err != nil {
+		return 0, "", err
+	}
+
+	// Parse the formula parameters
+	scaleFactor, err := strconv.ParseFloat(matches[4], 64)
+	if err != nil {
+		return 0, "", err
+	}
+	offsetAdjustment, err := strconv.ParseFloat(matches[5], 64)
+	if err != nil {
+		return 0, "", err
+	}
+	minValue, err := strconv.ParseFloat(matches[6], 64)
+	if err != nil {
+		return 0, "", err
+	}
+	maxValue, err := strconv.ParseFloat(matches[7], 64)
+	if err != nil {
+		return 0, "", err
+	}
 	unit := matches[8]
-
-	// Convert hex to binary
-	binaryData, err := convertHexToBinary(hexData)
-	if err != nil {
-		return 0, "", err
-	}
-	//print(binaryData)
-	//confirmed by rapidtables
-
-	// Extract bits
-	bits, err := extractBits(binaryData, startBit, lengthBits)
-	if err != nil {
-		return 0, "", err
-	}
-	//print(bits)
-	//confirmed by rapidtables
-
-	// Convert binary string to decimal
-	value, err := binaryStringToDecimal(bits)
-	if err != nil {
-		return 0, "", err
-	}
 
 	// Apply the formula
 	decodedValue := float64(value)*scaleFactor + offsetAdjustment
 
-	// Check range
+	// Validate the range
 	if decodedValue < minValue || decodedValue > maxValue {
 		return 0, "", fmt.Errorf("decoded value out of range: %.2f (expected range %.2f to %.2f)", decodedValue, minValue, maxValue)
 	}
@@ -87,13 +77,14 @@ func ExtractAndDecodeWithFormula(hexData string, formula string) (float64, strin
 
 func main() {
 	hexData := "7e80641a6000e2a2da"
-	formula := "48|20@0+ (0.1,0) [1|429496730] \"km\""
-	//formula := "31|32@0+ (0.1,0) [1|429496730] \"km\""
+	pid := "a6"
+	formula := "31|32@0+ (0.1,0) [1|4294967295] \"km\""
 
-	decodedValue, unit, err := ExtractAndDecodeWithFormula(hexData, formula)
+	decodedValue, unit, err := extractAndDecodeWithFormula(hexData, pid, formula)
 	if err != nil {
 		fmt.Println("Error:", err)
-	} else {
-		fmt.Printf("Decoded Value: %.2f %s\n", decodedValue, unit)
+		return
 	}
+
+	fmt.Printf("Decoded Value: %.2f %s\n", decodedValue, unit)
 }
